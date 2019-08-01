7f031116e8411b22c6d3ec1af47fd6ef7300ad4a
carvezho <47979962+carvezho@users.noreply.github.com>
2019-08-01 14:45:54 +0800

    Update uninitialized.h

--- Allocator/uninitialized.h
+++ Allocator/uninitialized.h
@@ -4,23 +4,23 @@
 #include "construct.h"
 #include "typeTraits.h"
 #include "stl_iterator.h"
-#include "stl_algobase.h"
+//#include "stl_algobase.h"
 
 namespace STL {
 
 	template <class ForwardIterator, class Size, class T>
 	inline ForwardIterator uninitialized_fill_n(ForwardIterator first, Size n, const T& x) {
-		using isPODtype = typename _type_traits<value_type_t<ForwardIterator> >::is_POD_type;
-		return _uninitialized_fill_n_aux(first, n, x, isPODtype());
+		using isPODtype = typename __type_traits<value_type_t<ForwardIterator> >::is_POD_type;
+		return __uninitialized_fill_n_aux(first, n, x, isPODtype());
 	}
 
 	template <class ForwardIterator, class Size, class T>
-	inline ForwardIterator _uninitialized_fill_n_aux(ForwardIterator first, Size n, const T& x, _true_type) {
+	inline ForwardIterator __uninitialized_fill_n_aux(ForwardIterator first, Size n, const T& x, __true_type) {
 		return STL::fill_n(first, n, x);
 	}
 
 	template <class ForwardIterator, class Size, class T>
-	inline ForwardIterator _uninitialized_fill_n_aux(ForwardIterator first, Size n, const T& x, _false_type) {
+	inline ForwardIterator __uninitialized_fill_n_aux(ForwardIterator first, Size n, const T& x, __false_type) {
 		ForwardIterator cur = first;
 		for (; n > 0; --n, ++cur)
 			construct(&*cur, x);
@@ -29,17 +29,17 @@ namespace STL {
 
 	template <class InputIterator, class ForwardIterator>
 	inline ForwardIterator uninitialized_copy(InputIterator first, InputIterator last, ForwardIterator result) {
-		using isPODtype = typename _type_traits<value_type_t<InputIterator> >::is_POD_type;
-		return _uninitialized_copy_aux(first, last, result, isPODtype());
+		using isPODtype = typename __type_traits<value_type_t<InputIterator> >::is_POD_type;
+		return __uninitialized_copy_aux(first, last, result, isPODtype());
 	}
 
 	template <class InputIterator, class ForwardIterator>
-	inline ForwardIterator _uninitialized_copy_aux(InputIterator first, InputIterator last, ForwardIterator result, _true_type) {
+	inline ForwardIterator __uninitialized_copy_aux(InputIterator first, InputIterator last, ForwardIterator result, __true_type) {
 		return STL::copy(first, last, result);
 	}
 
 	template <class InputIterator, class ForwardIterator>
-	inline ForwardIterator _uninitialized_copy_aux(InputIterator first, InputIterator last, ForwardIterator result, _false_type) {
+	inline ForwardIterator __uninitialized_copy_aux(InputIterator first, InputIterator last, ForwardIterator result, __false_type) {
 		ForwardIterator cur = result;
 		for (; first != last; ++first, ++cur)
 			construct(&*cur, *first);
@@ -58,17 +58,17 @@ namespace STL {
 
 	template <class ForwardIterator, class T>
 	inline void uninitialized_fill(ForwardIterator first, ForwardIterator last, const T& x) {
-		using isPODtype = typename _type_traits<value_type_t<ForwardIterator> >::is_POD_type;
-		_uninitialized_fill_aux(first, last, x, isPODtype());
+		using isPODtype = typename __type_traits<value_type_t<ForwardIterator> >::is_POD_type;
+		__uninitialized_fill_aux(first, last, x, isPODtype());
 	}
 
 	template <class ForwardIterator, class T>
-	inline void _uninitialized_fill(ForwardIterator first, ForwardIterator last, const T& x, _true_type) {
+	inline void __uninitialized_fill(ForwardIterator first, ForwardIterator last, const T& x, __true_type) {
 		STL::fill(first, last, x);
 	}
 
 	template <class ForwardIterator, class T>
-	inline void _uninitialized_fill(ForwardIterator first, ForwardIterator last, const T& x, _false_type) {
+	inline void __uninitialized_fill(ForwardIterator first, ForwardIterator last, const T& x, __false_type) {
 		ForwardIterator cur;
 		for (; cur != last; ++cur)
 			construct(&*cur, x);
