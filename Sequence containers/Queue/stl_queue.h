dcd1763ddb519f91bf80933287aaa56a71099f2b
carvezho <47979962+carvezho@users.noreply.github.com>
2019-08-09 20:23:28 +0800

    Update stl_queue.h

--- Sequence containers/Queue/stl_queue.h
+++ Sequence containers/Queue/stl_queue.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include "stl_deque.h"
+#include "stl_vector.h"
+#include "stl_function.h"
+#include "heap_algorithm.h"
 
 namespace STL {
 
@@ -39,4 +42,44 @@ namespace STL {
 	bool operator<(const queue<T, Sequence>& x, const queue<T, Sequence>& y) {
 		return x.c < y.c;
 	}
+
+	template <class T, class Sequence = vector<T>, class Compare = less<typename Sequence::value_type>>
+	class priority_queue {
+	public:
+		using value_type = typename Sequence::value_type;
+		using size_type = typename Sequence::size_type;
+		using reference = typename Sequence::reference;
+		using const_reference = typename Sequence::const_reference;
+	private:
+		Sequence c;
+		Compare comp;
+	public:
+		priority_queue() : c() { }
+		explicit priority_queue(const Compare& x) : c(), comp(x) { }
+
+		template <class InputIterator>
+		priority_queue(InputIterator first, InputIterator last, const Compare& x)
+			: c(first, last) { make_heap(c.begin(), c.end(), comp); }
+		template <class InputIterator>
+		priority_queue(InputIterator first, InputIterator last)
+			: c(first, last) { make_heap(c.begin(), c.end(), comp); }
+
+		bool empty() const noexcept { return c.empty(); }
+		size_type size() const noexcept { return c.size(); }
+		const_reference top() const noexcept { return c.front(); }
+		void push(const value_type& x) {
+			__STL_TRY{
+				c.push_bakc(x);
+				push_heap(c.begin(), c.end(), comp);
+			}
+			__STL_UNWIND(c.clear());
+		}
+		void pop(const value_type& x) {
+			__STL_TRY{
+				pop_heap(c.begin(), c.end(), comp)
+				c.pop_back(x);
+			}
+			__STL_UNWIND(c.clear());
+		}
+	};
 }
