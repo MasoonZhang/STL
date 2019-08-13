8d6cadcb4985f281ad1c866c7abaa925c7fb517a
carvezho <47979962+carvezho@users.noreply.github.com>
2019-08-13 20:21:13 +0800

    Update stl_multimap.h

--- Associative containers/Multimap/stl_multimap.h
+++ Associative containers/Multimap/stl_multimap.h
@@ -1,127 +1,121 @@
 #pragma once
 
-#include "rb_tree.h"
-#include "stl_function.h"
+#include "hash_func.h"
+#include "hashtable.h"
 
 namespace STL {
 
-	template <class Key, class T, class Compare = less<key>, class Alloc = alloc>
-	class multimap {
-		using key_type = Key;
-		using data_type = T;
-		using mapped_type = T;
-		using value_type = pair<const Key, T>;
-		using key_compare = Compare;
-
-		class value_compare : public binary_function<value_type, value_type, bool> {
-			friend class map<Key, T, Compare, Alloc>;
-		protected:
-			Compare comp;
-			value_compare(Compare c) : comp(c) { }
-		public:
-			bool operator()(const value_type& x, const value_type& y) const {
-				return comp(x.first, y.first);
-			}
-		};
-
+	template <class Key, class T, class HashFcn = hash<Key>, class EqualKey = equal_to<Key>, class Alloc = simpleAlloc<Key> >
+	class hash_multimap {
 	private:
-		using rep_type = rb_tree<key_type, value_type, select1st<value_type>, key_compare, Alloc>;
-		rep_type t;
+		using ht = hashtable<pair<const Key, T>, Key, HashFcn, select1st<pair<const Key, T> >, EqualKey, Alloc>;
+		ht rep;
 
 	public:
-		using pointer = typename rep_type::pointer;
-		using const_pointer = typename rep_type::const_pointer;
-		using reference = typename rep_type::reference;
-		using const_reference = typename rep_type::const_reference;
-		using iterator = typename rep_type::iterator;
-		using const_iterator = typename rep_type::const_iterator;
-		using reverse_iterator = typename rep_type::reverse_iterator;
-		using const_reverse_iterator = typename rep_type::const_reverse_iterator;
-		using size_type = typename rep_type::size_type;
-		using difference_type = typename rep_type::difference_type;
+		using key_type = typename ht::key_type;
+		using data_type = T;
+		using mapped_type = T;
+		using value_type = typename ht::value_type;
+		using hasher = typename ht::hasher;
+		using key_equal = typename ht::key_equal;
 
-		map() : t(Compare()) { }
-		explicit multimap(const Compare& comp) : t(comp) { }
+		using size_type = typename ht::size_type;
+		using differece_type = typename ht::differece_type;
+		using pointer = typename ht::pointer;
+		using const_pointer = typename ht::const_pointer;
+		using reference = typename ht::reference;
+		using const_reference = typename ht::const_reference;
+
+		using iterator = typename ht::iterator;
+		using const_iterator = typename ht::const_iterator;
+
+		hasher hash_funct() const noexcept { return rep.hash_funct(); }
+		key_equal key_eq() const noexcept { return rep.key_eq(); }
+
+	public:
+		hash_multimap() : rep(100, hasher(), key_equal()) { }
+		explicit hash_multimap(size_type n) : rep(n, hasher(), key_equal()) { }
+		hash_multimap(size_type n, const hasher& hf) : rep(n, hf, key_equal()) { }
+		hash_multimap(size_type n, const hasher& hf, const key_equal& eql) : rep(n, hf, eql) { }
 
 		template <class InputIterator>
-		multimap(InputIterator first, InputIterator last) : t(Compare()) {
-			t.insert_equal(first, last);
-		}
-
-		template <class InputIterator>
-		multimap(InputIterator first, InputIterator last, const Compare& comp) : t(comp) {
-			t.insert_equal(first, last);
-		}
-
-		multimap(const map<Key, T, Compare, Alloc>& x) : t(x.t) { }
-		multimap(const map<Key, T, Compare, Alloc>& operator=(const map(const map<Key, T, Compare, Alloc>& x) {
-			t = x.t;
-			return *this;
-		}
-
-		key_compare key_comp() const noexcept { return t.key_comp(); }
-		value_compare value_comp() const noexcept {
-			return value_compare(t.key_comp());
-		}
-		iterator begin() noexcept { return t.begin(); }
-		iterator end() noexcept { return t.end(); }
-		const_iterator begin() const noexcept { return t.cbegin(); }
-		const_iterator end() const noexcept { return t.cend(); }
-		const_reverse_iterator crbegin() const noexcept { return t.rbegin(); }
-		const_reverse_iterator crend() const noexcept { return t.rend(); }
-		bool empty() const noexcept { return t.empty(); }
-		size_type size() const noexcept { return t.size(); }
-		size_type max_size() const noexcept { return t.max_size(); }
-		T& operator[](const key_type& k) {
-			return (*(insert(value_type(k, T()))).first()).second;
-		}
-		void swap(map<Key, T, Compare, Alloc>& x) noexcept { t.swap(x.t); }
-
-		pair<iterator, bool> insert(const value_type& x) {
-			return t.insert_equal(x);
-		}
-		iterator insert(iterator position, const value_type& x) {
-			return t.insert_equal(position, x);
+		hash_multimap(InputIterator f, InputIterator l)
+			: rep(100, hasher(), key_equal()) {
+			rep.insert_equal(f, l);
 		}
 		template <class InputIterator>
-		void insert(InputIterator first, InputIterator last) {
-			t.insert_equal(first, last);
+		hash_multimap(InputIterator f, InputIterator l, size_type n)
+			: rep(n, hasher(), key_equal()) {
+			rep.insert_equal(f, l);
+		}
+		template <class InputIterator>
+		hash_multimap(InputIterator f, InputIterator l, size_type n, const hasher& hf)
+			: rep(n, hf, key_equal()) {
+			rep.insert_equal(f, l);
+		}
+		template <class InputIterator>
+		hash_multimap(InputIterator f, InputIterator l, size_type n, const hasher& hf, const key_equal& eql)
+			: rep(n, hf, eql()) {
+			rep.insert_equal(f, l);
 		}
 
-		void erase(iterator position) { t.erase(position); }
-		size_type erase(const key_type& x) { return t.erase(x); }
-		void erase(iterator first, iterator last) { t.erase(first, last); }
-		void clear() { t.clear(); }
+	public:
+		size_type size() const noexcept { return rep.size(); }
+		size_type max_size() const noexcept { return rep.max_size(); }
+		bool empty() const noexcept { return rep.empty(); }
+		void swap(hash_multimap& ht) noexcept { rep.swap(ht.rep); }
+		friend bool operator==(const hash_multimap&, const hash_multimap&);
 
-		iterator find(const key_type& x) noexcept { return t.find(x); }
-		const_iterator find(const key_type& x) const noexcept { return t.find(x); }
-		size_type count(const key_type& x) const noexcept { return t.count(x); }
-		iterator lower_bound(const key_type& x) noexcept { return t.lower_bound(x); }
-		const_iterator lower_bound(const key_type& x) const noexcept {
-			return t.lower_bound(x);
+		iterator begin() const noexcept { return rep.begin(); }
+		iterator end() const noexcept { return rep.end(); }
+		const_iterator begin() const noexcept { return rep.begin(); }
+		const_iterator end() const noexcept { return rep.end(); }
+
+	public:
+		pair<iterator, bool> insert(const value_type& obj) {
+			return rep.insert_equal(obj);
 		}
-		iterator upper_bound(const key_type& x) noexcept { return t.upper_bound(x); }
-		const_iterator upper_bound(const key_type& x) const noexcept {
-			return t.upper_bound(x);
+		template <class InputIterator>
+		void insert(InputIterator f, InputIterator l) {
+			rep.insert_equal(f, l);
+		}
+		pair<iterator, bool> insert_noresize(const value_type& obj) {
+			return rep.insert_equal_noresize(obj);
 		}
 
-		pair<iterator, iterator> equal_range(const key_type& x) noexcept {
-			return t.equal_range(x);
+		iterator find(const key_type& key) const { return rep.find(key); }
+		const_iterator find(const key_type& key) const { return rep.find(key); }
+
+		/*T& operator[](const key_type& key) {
+			return rep.find_or_insert(value_type(key, T())).second;
+		}*/
+
+		size_type count(const key_type& key) const { return rep.count(key); }
+
+		pair<iterator, iterator> equal_range(const key_type& key) const {
+			return rep.equal_range(key);
 		}
-		pair<const_iterator, const_iterator> equal_range(const key_type& x) const noexcept {
-			return t.equal_range(x);
+		pair<const_iterator, iterator> equal_range(const key_type& key) const {
+			return rep.equal_range(key);
 		}
-		friend bool operator==(const map&, const map&);
-		friend bool operator<(const map&, const map&);
+
+		size_type erase(const key_type& key) { return rep.erase(key); }
+		void erase(iterator it) { rep.erase(it); }
+		void erase(iterator f, iterator l) { rep.erase(f, l); }
+		void clear() { rep.clear(); }
+
+	public:
+		void resize(size_type n) { rep.resize(n); }
+		size_type bucket_count() const noexcept { return rep.bucket_count(); }
+		size_type max_bucket_count() const noexcept { return rep.max_bucket_count(); }
+		/*size_type elems_in_bucket(size_type n) const noexcept {
+			return rep.elems_in_bucket(n);
+		}*/
 	};
 
-	template <class Key, class Tp, class Compare, class Alloc>
-	inline bool operator==(const multimap<Key, Tp, Compare, Alloc>& x, const multimap<Key, Tp, Compare, Alloc>& y) {
-		return x.t == y.t;
-	}
-
-	template <class Key, class Tp, class Compare, class Alloc>
-	inline bool operator<(const multimap<Key, Tp, Compare, Alloc>& x, const multimap<Key, Tp, Compare, Alloc>& y) {
-		return x.t < y.t;
+	template <class Key, class T, class HashFcn, class EqualKey, class Alloc>
+	inline bool operator==(const hash_multimap<Key, T, HashFcn, EqualKey, Alloc>& lhs,
+		const hash_multimap<Key, T, HashFcn, EqualKey, Alloc>& rhs) {
+		return lhs.rep == rhs.rep;
 	}
 }
